a852acf50ecedb3afdc91d0110a984b3449e2637
manohar singh rajawat <manoharsingh1920@gmail.com>
2020-02-20 12:28:06 +0530

    Update MeetingRoomPersonDetector.py

--- MeetingRoomPersonDetector.py
+++ MeetingRoomPersonDetector.py
@@ -3,53 +3,70 @@
 #Import packages from libraries
 from salesforce_connection import SalesforceApp
 from human_detector import Detector
-import cv2 as cv
+from imutils.video import VideoStream
 import time
 import sys
 
+
+#Defining Constants
+
+VACANT = 0
+OCCUPIED = 1
+
 if __name__ == "__main__":
 #Calling salesforce class
+ MeetingRoomId = "2001"
  sf = SalesforceApp()
  isConnected = sf.getStatusCode();
  if (isConnected == 200):
   print ("[INFO] Salesforce Connection Done !!!")
-  detector = Detector(0.5);
+  detector = Detector(0.6);
 
 #Initialize Camera And Warming it up
-  cap = cv.VideoCapture(0)
+  cap = VideoStream(usePiCamera=1).start()
   time.sleep(1.5)
 
   print ("[INSIDE] Going inside while loop !!!")
 
   try:
-   PreviousStateEquipped = False
-   RoomEquipped = False
-   count = 0;
-   lst = [];
+   WINDOW_DETECTION = 5
+   PRESENCE_THRESHOLD = 3
+   VACANCY_THRESHOLD = 1
+   ROOM = VACANT
+   COUNTER_VACANCY = 0
    while True:
-     if count % 5 == 0:
-      print ("[LIST] : ",lst)
-      if len(lst):
-       if lst.count(1)/len(lst) > 0.5:
-        RoomEquipped = True
-       else:
-        RoomEquipped = False
-       sf.publishEvent(RoomEquipped,2001)
-       lst.clear()
-       print ("[LIST] List cleared !!!")
+     #INFINITE LOOP TO CHECK THE STATUS
+     DETECTION_VAR = 0
+     COUNTER_DETECTION = 0
+     while DETECTION_VAR < WINDOW_DETECTION:
+      time.sleep(1)
+      frame = cap.read()
+      frame_2 = detector.detect(frame)
+      if frame_2 > 0:
+       COUNTER_DETECTION += 1
+       COUNTER_VACANCY = 0
+      DETECTION_VAR += 1
+     print("CounterDetection",COUNTER_DETECTION);
+     if ROOM == VACANT and COUNTER_DETECTION >= PRESENCE_THRESHOLD:
+      ROOM = OCCUPIED
+      sf.publishEvent(True,MeetingRoomId);
+      print ("The Room has been set as OCCUPIED");
 
-     time.sleep(2) #Wait for 1 second to reduce load
-     _,frame = cap.read()
-     frame_2 = detector.detect(frame)
-     count += 1
-     lst.append(1) if frame_2 > 0 else lst.append(0)
-     print (" ")
-     print ("[OBJECT_COUNT] Objects count is : ",frame_2)
-     print (" ")
+     if COUNTER_DETECTION < PRESENCE_THRESHOLD:
+      COUNTER_VACANCY += 1
+
+     if ROOM == OCCUPIED and COUNTER_VACANCY == VACANCY_THRESHOLD:
+      ROOM = VACANT
+      sf.publishEvent(False,MeetingRoomId);
+      print ("The Room has been set as VACANT");
   except KeyboardInterrupt:
+   if ROOM == OCCUPIED:
+    ROOM = VACANT
+    sf.publishEvent(False,MeetingRoomId)
+    print ("The Room has been set as VACANT");
+
    print ("You pressed CTRL+C !")
-   cap.release()
-   cv.destroyAllWindows()
+   cap.stop()
    print ("Cleanup Done !!!")
    sys.exit(0)
 
